85d1e04a0de10f1ea7daf77a029ab8f8678f8921
leonhano <haohan819@gmail.com>
2018-02-26 02:04:08 -0500

    - add collider

--- Assets/Cartoon SportCar B01/Standard Assets/script/CarUserControl.cs
+++ Assets/Cartoon SportCar B01/Standard Assets/script/CarUserControl.cs
@@ -9,14 +9,19 @@ namespace UnityStandardAssets.Vehicles.Car
     public class CarUserControl : MonoBehaviour
     {
         private CarController m_Car; // the car controller we want to use
-
+        private GameObject m_carGO;
+        private bool m_bStopToCharge;    //stop to charge
+        private float m_targetTime = 3.0f;
+        public float m_CHARGETIME = 3f;
 
         private void Awake()
         {
             // get the car controller
             m_Car = GetComponent<CarController>();
+            m_carGO = m_Car.gameObject;
+            m_bStopToCharge = false;
+            m_targetTime = m_CHARGETIME;
         }
-
 
         private void OriginalControl()
         {
@@ -33,8 +38,8 @@ namespace UnityStandardAssets.Vehicles.Car
         }
         private void AutoDrive()
         {
-            float h = UnityEngine.Random.value;//CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = UnityEngine.Random.value; //CrossPlatformInputManager.GetAxis("Vertical");
+            float h = UnityEngine.Random.Range(0, 100);//CrossPlatformInputManager.GetAxis("Horizontal");
+            float v = UnityEngine.Random.Range(0, 100); //CrossPlatformInputManager.GetAxis("Vertical");
 #if !MOBILE_INPUT
             float handbrake = 0f; // CrossPlatformInputManager.GetAxis("Jump");
             //Debug.Log("h = " + h + " y =" + v + " handbrake = " + handbrake);
@@ -44,10 +49,52 @@ namespace UnityStandardAssets.Vehicles.Car
 #endif
         }
 
+        private void StopDrive()
+        {
+            m_Car.Move(0, 0, 0, 1000);
+        }
+        
+        public void StartToCharge()
+        {
+            m_bStopToCharge = true;
+            m_targetTime = m_CHARGETIME;
+            StopDrive();
+        }
+
+        public void FinishToCharge()
+        {
+            m_bStopToCharge = false;
+            //m_targetTime = CHARGETIME;
+            Debug.Log(this.gameObject.name + "   --- Finish to charge  ----");
+        }
+
         private void FixedUpdate()
         {
-            //OriginalControl();
-            AutoDrive();
+            /*
+            m_targetTime -= Time.deltaTime;
+
+            if (m_targetTime <= 0.0f)
+            {
+
+                FinishToCharge();
+
+            }
+            */
+
+            if (m_bStopToCharge)
+                Invoke("FinishToCharge", 30);
+
+            if (!m_bStopToCharge)
+            {
+                //OriginalControl();
+                AutoDrive();
+            }
+            else
+            {
+                StopDrive();
+            }
         }
+
+
     }
 }
---
+++ Assets/CollisionDetector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class CollisionDetector : MonoBehaviour {
+   
+    private CarUserControl m_CarControl;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag != "Ground")
+        {
+            m_CarControl = this.gameObject.GetComponentInParent<CarUserControl>();
+            m_CarControl.StartToCharge();
+            Debug.Log(this.gameObject.name + "   --- StartToCharge to charge  ----  " + other.gameObject.name);
+        }
+
+        if (other.gameObject.tag == "Car")
+        {
+
+            Debug.Log(this.gameObject.name + "   --- Car to charge  ----  " + other.gameObject.name);
+
+        }
+
+    }
+}
